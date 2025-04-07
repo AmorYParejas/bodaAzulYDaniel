367b3ba7f404a48af3a7ba1e6e3b0dfd40407332
David Hernandez <David.arcos1000@gmail.com>
2025-04-07 12:02:44 -0600

    correcciones

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
    // Countdown Timer
-   const targetDate = new Date('2025-02-21T12:17:00');
+   const targetDate = new Date('2025-07-19T12:15:30');
    const countdownElement = document.getElementById('countdown');
 
    function updateCountdown() {
@@ -20,19 +20,19 @@
 
            
            const daysElement = document.createElement('p');
-           daysElement.textContent = `${days} Días : `;
+           daysElement.textContent = `Días: ${days}`;
            countdownElement.appendChild(daysElement);
 
            const hoursElement = document.createElement('p');
-           hoursElement.textContent = ` ${hours} Horas : `;
+           hoursElement.textContent = `Horas: ${hours}`;
            countdownElement.appendChild(hoursElement);
 
            const minutesElement = document.createElement('p');
-           minutesElement.textContent = `${minutes} Minutos : `;
+           minutesElement.textContent = `Minutos: ${minutes}`;
            countdownElement.appendChild(minutesElement);
 
            const secondsElement = document.createElement('p');
-           secondsElement.textContent = `${seconds} Segundos`;
+           secondsElement.textContent = `Segundos: ${seconds}`;
            countdownElement.appendChild(secondsElement);
        } else {
            // Mostrar mensaje cuando la fecha haya llegado
@@ -44,31 +44,14 @@
 
    setInterval(updateCountdown, 1000);
 
-       // Slider Functionality
-       const slides = document.querySelector('.slides');
-       const images = document.querySelectorAll('.slides img');
-       let currentIndex = 0;
 
-       document.getElementById('next').addEventListener('click', () => {
-           currentIndex = (currentIndex + 1) % images.length;
-           slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-       });
-
-       document.getElementById('prev').addEventListener('click', () => {
-           currentIndex = (currentIndex - 1 + images.length) % images.length;
-           slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-       });
-
-
-
-
-
+//Funcionalidad del  formulario
   document.getElementById('whatsappForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evitar que se envíe el formulario de forma tradicional
 
     // Obtener los valores de los campos
     const name = document.getElementById('name').value;
-    const phone = 7471594702;
+    const phone = 7472224438;
 
     // Crear el mensaje para WhatsApp
     const whatsappMessage = `Hola, somos la familia ${encodeURIComponent(name)}.  Mensaje: Recibimos tu invitación y estaremos felices de acompañarlos`;
@@ -80,3 +63,31 @@
     window.open(whatsappURL, '_blank');
   });
 
+
+//Modales
+
+function abrirModal(numero){
+    if(numero == 1){
+        const modal1 = document.getElementById("modal1");
+        modal1.classList.remove("ocultar-modal");
+        modal1.classList.add("modal");
+    }
+    if(numero == 2){
+        const modal2 = document.getElementById("modal2");
+        modal2.classList.remove("ocultar-modal");
+        modal2.classList.add("modal");
+    }
+}
+
+function cerrarModal(numero){
+    if(numero == 1){
+        const modal1 = document.getElementById("modal1");
+        modal1.classList.remove("modal");
+        modal1.classList.add("ocultar-modal");
+    }
+    if(numero == 2){
+        const modal2 = document.getElementById("modal2");
+        modal2.classList.remove("modal");
+        modal2.classList.add("ocultar-modal");
+    }
+}
